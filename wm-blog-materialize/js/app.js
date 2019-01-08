27e31c8247572eb6af0a79bc09c102cef54f4515
welisonmenezes <welisonmenezes@hotmail.com>
2019-01-08 14:59:03 -0200

    finish header mobile

--- wm-blog-materialize/js/app.js
+++ wm-blog-materialize/js/app.js
@@ -1,1 +1,15 @@
 $('.dropdown-trigger').dropdown();
+
+$('.navbar-toggler').on('click', function(){
+	var nav = $('#navbarMain');
+
+	if(nav.hasClass('opened')){
+		nav.slideUp(300, function(){
+			nav.removeClass('opened');
+		});
+	}else{
+		nav.slideDown(300, function() {
+			nav.addClass('opened');
+		});
+	}
+});
