da8ecc6326eaf3289445f1c9a5b39f15c6ccb8c7
welisonmenezes <welisonmenezes@hotmail.com>
2019-01-11 12:22:25 -0200

    materialize - carousel and somethings

--- wm-blog-materialize/js/app.js
+++ wm-blog-materialize/js/app.js
@@ -1,8 +1,9 @@
+// init dropdown
 $('.dropdown-trigger').dropdown();
 
+// init toogle navbar (responsive small)
 $('.navbar-toggler').on('click', function(){
 	var nav = $('#navbarMain');
-
 	if(nav.hasClass('opened')){
 		nav.slideUp(300, function(){
 			nav.removeClass('opened');
@@ -14,7 +15,29 @@ $('.navbar-toggler').on('click', function(){
 	}
 });
 
+// inti modal
 $('.modal').modal({
 	startingTop: '5%',
 	endingTop: '5%'
+});
+
+// init carousel
+$('.carousel').carousel({
+	dist: 0,
+	numVisible: 1,
+	fullWidth: true,
+	indicators: false,
+	noWrap: true
+});
+$('.carousel-wrap .btn-next-prev.prev').on('click', function(){
+	var car = $(this).parent().find('.carousel');
+	if(car){
+		car.carousel('prev');
+	}
+});
+$('.carousel-wrap .btn-next-prev.next').on('click', function(){
+	var car = $(this).parent().find('.carousel');
+	if(car){
+		car.carousel('next');
+	}
 });
