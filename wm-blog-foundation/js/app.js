293377a3f3910c0b744e63b5a656ba57e0e78194
welisonmenezes <welisonmenezes@hotmail.com>
2019-01-14 13:53:14 -0200

    foundation - header mobile

--- wm-blog-foundation/js/app.js
+++ wm-blog-foundation/js/app.js
@@ -1,1 +1,33 @@
 var elem = new Foundation.DropdownMenu($('.nav-main.dropdown.menu'), {});
+
+
+// init toogle navbar (responsive small)
+$('.navbar-toggler').on('click', function(){
+	var nav = $('#navbarMain');
+	if(nav.hasClass('opened')){
+		nav.slideUp(300, function(){
+			nav.removeClass('opened');
+		});
+	}else{
+		nav.slideDown(300, function() {
+			nav.addClass('opened');
+		});
+	}
+});
+
+
+$('.nav-main li').on('click', function(){
+	var windowWidth = window.innerWidth || $(window).width();
+	if(windowWidth > 810) return; 
+	var t = $(this);
+	var nav = t.find('.menu');
+	if(t.hasClass('opened')){
+		nav.slideUp(300, function(){
+			t.removeClass('opened');
+		});
+	}else{
+		t.slideDown(300, function() {
+			t.addClass('opened');
+		});
+	}
+});
