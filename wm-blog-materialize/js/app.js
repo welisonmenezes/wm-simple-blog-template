304f11c8fecbd874ebd5d293f9e4b7580004fd92
welisonmenezes <welisonmenezes@hotmail.com>
2019-01-17 12:30:19 -0200

    materialize - add window fade in on load effect

--- wm-blog-materialize/js/app.js
+++ wm-blog-materialize/js/app.js
@@ -1,3 +1,31 @@
+$(window).on('load', function () {
+
+	// init carousel
+	$('.carousel').carousel({
+		dist: 0,
+		numVisible: 1,
+		fullWidth: true,
+		indicators: false,
+		noWrap: true
+	});
+	$('.carousel-wrap .btn-next-prev.prev').on('click', function(){
+		var car = $(this).parent().find('.carousel');
+		if(car){
+			car.carousel('prev');
+		}
+	});
+	$('.carousel-wrap .btn-next-prev.next').on('click', function(){
+		var car = $(this).parent().find('.carousel');
+		if(car){
+			car.carousel('next');
+		}
+	});
+
+    setTimeout(function () {
+        $('body').fadeTo(300, 1);
+    }, 500);
+});
+
 // init dropdown
 $('.dropdown-trigger').dropdown();
 
@@ -19,25 +47,4 @@ $('.navbar-toggler').on('click', function(){
 $('.modal').modal({
 	startingTop: '5%',
 	endingTop: '5%'
-});
-
-// init carousel
-$('.carousel').carousel({
-	dist: 0,
-	numVisible: 1,
-	fullWidth: true,
-	indicators: false,
-	noWrap: true
-});
-$('.carousel-wrap .btn-next-prev.prev').on('click', function(){
-	var car = $(this).parent().find('.carousel');
-	if(car){
-		car.carousel('prev');
-	}
-});
-$('.carousel-wrap .btn-next-prev.next').on('click', function(){
-	var car = $(this).parent().find('.carousel');
-	if(car){
-		car.carousel('next');
-	}
 });
