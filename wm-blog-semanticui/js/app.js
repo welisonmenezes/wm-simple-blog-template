559aea68546635569147b99f3fabe854f944bfd2
welisonmenezes <welisonmenezes@hotmail.com>
2019-01-18 11:31:19 -0200

    semantic - responsive header

--- wm-blog-semanticui/js/app.js
+++ wm-blog-semanticui/js/app.js
@@ -3,3 +3,32 @@ $(window).on('load', function () {
         $('body').fadeTo(300, 1);
     }, 500);
 });
+
+// init toogle navbar (responsive small)
+$('.navbar-toggler').on('click', function(){
+	var nav = $('#navbarMain');
+	if(nav.hasClass('opened')){
+		nav.slideUp(300, function(){
+			nav.removeClass('opened');
+		});
+	}else{
+		nav.slideDown(300, function() {
+			nav.addClass('opened');
+		});
+	}
+});
+
+$('.menu-dropdown > a').on('click', function() {
+	var t = $(this);
+	var p = t.parent();
+	var menu = p.find('.menu');
+
+	if (menu) {
+		if (p.hasClass('opened')) {
+			p.removeClass('opened');
+		} else {
+			p.addClass('opened');
+		}
+	}
+	return false;
+});
